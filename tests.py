2b2a873d478283d66566be5e2e7dd697afe5e832
Fredrik Gustafsson <fredrikgson@gmail.com>
2018-04-26 11:08:33 +0200

    Adding test for segmentation

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ import unittest
 
 import numpy as np
 
-from hw2_binary_reconstruction import extract_sub_images
-from hw2_binary_reconstruction import merge_subimages
+from binary_reconstruction import extract_sub_images
+from binary_reconstruction import merge_subimages
 
 import sys, os
 
@@ -13,34 +13,27 @@ class BinaryReconstruction(unittest.TestCase):
         with open(os.devnull, "w") as devnull:
             old_stdout = sys.stdout
             #sys.stdout = devnull
-            for dim in range(20, 60):
-                for sub_image_size in [8,9,10]:
+            for dim in range(20, 60, 10):
+                for sub_image_size in [8,9,10, 15]:
                     if sub_image_size > dim - 4:
                         continue
                     for overlap in [1,2,3]:
-                        print("dim = {}, sub_image_size = {}, overlap = {}".format(dim, sub_image_size, overlap)) 
                         image = np.arange(dim**2).reshape(dim,dim)
                         x,y = image.shape
 
-                        print(image)
-                        print(image.shape)
-        
                         sub_images = extract_sub_images(image, sub_image_size, overlap)
                         first_subimage_true = image[0:sub_image_size, 0:sub_image_size]
                         first_subimage = sub_images[0][0]
                         last_sub_image_true = image[x - sub_image_size:x, y - sub_image_size:y]
                         last_sub_image = sub_images[-1][0]
 
-                        print("dim = {}, sub_image_size = {}, overlap = {}".format(dim, sub_image_size, overlap)) 
                         self.assertTrue(np.allclose(first_subimage_true, first_subimage))
-                        #self.assertTrue(np.allclose(last_sub_image_true, last_sub_image))
-        
+
+                        # Recreate orignal images from subimages
                         stitched_image = merge_subimages(sub_images, image.shape)
-
+                        # Make sure stitched image
                         self.assertTrue(np.allclose(stitched_image, image))
-                        print("OOOK!")
             sys.stdout = old_stdout
-
 
 
 if __name__ == '__main__':
